bc5718eafdcbb4f63011ba0cb0c1525ff0b2d79f
Jazib Aslam <jazibaslam124@gmail.com>
2025-05-05 16:34:04 +0200

    Add model.py

---
+++ model.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CNNModel(nn.Module):
+    def __init__(self):
+        super(CNNModel, self).__init__()
+        self.conv1 = nn.Conc2d(1, 32, 3, padding = 1 )
+        self.conv2 = nn.Conc2d(32, 64, 3, padding = 1 )
+        self.fc1 = nn.Linear(64 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 64 * 7 * 7)
+        x = F.relu(self.fc1(x))
+        return self.fc2(x)
